8d3517ce1bbecfe3f1cb3189fab306534762b300
Lisak <liskamates@seznam.cz>
2024-06-09 15:33:59 +0200

    Finally fixed movement

--- src/com/DodgeAndLast/KeyInput.java
+++ src/com/DodgeAndLast/KeyInput.java
@@ -6,11 +6,15 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter {
 
     private Handler handler;
+    private boolean[] keyDown = new boolean[4];
 
     public KeyInput(Handler handler){
         this.handler = handler;
+        keyDown[0] = false;
+        keyDown[1] = false;
+        keyDown[2] = false;
+        keyDown[3] = false;
     }
-
 
     // player´s movement
     public void keyPressed(KeyEvent e){
@@ -19,10 +23,10 @@ public class KeyInput extends KeyAdapter {
         for(int i = 0; i < handler.object.size(); i++){
             GameObject tempObject = handler.object.get(i);
             if (tempObject.getId() == ID.Player){
-                if (key == KeyEvent.VK_D) tempObject.setVelX(5);
-                if (key == KeyEvent.VK_A) tempObject.setVelX(-5);
-                if (key == KeyEvent.VK_W) tempObject.setVelY(-5);
-                if (key == KeyEvent.VK_S) tempObject.setVelY(5);
+                if (key == KeyEvent.VK_D) {tempObject.setVelX(5); keyDown[0] = true; }
+                if (key == KeyEvent.VK_A) {tempObject.setVelX(-5); keyDown[1] = true; }
+                if (key == KeyEvent.VK_W) {tempObject.setVelY(-5); keyDown[2] = true; }
+                if (key == KeyEvent.VK_S) {tempObject.setVelY(5); keyDown[3] = true; }
             }
 
         }
@@ -35,10 +39,16 @@ public class KeyInput extends KeyAdapter {
         for(int i = 0; i < handler.object.size(); i++){
             GameObject tempObject = handler.object.get(i);
             if (tempObject.getId() == ID.Player){
-                if (key == KeyEvent.VK_D) tempObject.setVelX(0);
-                if (key == KeyEvent.VK_A) tempObject.setVelX(0);
-                if (key == KeyEvent.VK_W) tempObject.setVelY(0);
-                if (key == KeyEvent.VK_S) tempObject.setVelY(0);
+                if (key == KeyEvent.VK_D) keyDown[0] = false;
+                if (key == KeyEvent.VK_A) keyDown[1] = false;
+                if (key == KeyEvent.VK_W) keyDown[2] = false;
+                if (key == KeyEvent.VK_S) keyDown[3] = false;
+
+                //vertical movement
+                if(!keyDown[2] && !keyDown[3]) tempObject.setVelY(0);
+
+                //horizontal movement
+                if(!keyDown[0] && !keyDown[1]) tempObject.setVelX(0);
             }
 
         }
